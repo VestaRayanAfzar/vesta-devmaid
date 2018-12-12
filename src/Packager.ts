20004ddf28f89c4f8e308d633a976da22c82925f
hbtb <bishehsary@gmail.com>
2018-12-12 01:19:07 -0600

    starting bug fix

--- src/Packager.ts
+++ src/Packager.ts
@@ -7,7 +7,7 @@ const rimraf = require("rimraf");
 
 export type Transformer = (config: any, target: string, isProduction: boolean) => any;
 
-export interface IPackagerConfig {
+export default interface IPackagerConfig<T> {
     root: string;
     src: string;
     targets: string[];
@@ -25,7 +25,7 @@ export class Packager {
     private distBase = "vesta";
     private mainTarget = "";
 
-    constructor(private config: IPackagerConfig) {
+    constructor(private config: IPackagerConfig<any>) {
     }
 
     public createTasks() {
